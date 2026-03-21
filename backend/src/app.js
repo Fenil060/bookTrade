5719f74119be094d536f2b92c3a8a8f0b20bc2fd
Fenil060 <gadhiyafenil090@gmail.com>
2026-03-21 14:33:27 +0530

    fix deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://book-trade-daoj.vercel.app/", // your Vite frontend URL
+  origin: "https://book-trade-daoj.vercel.app", // your Vite frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
