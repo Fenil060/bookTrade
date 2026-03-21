20a69fb6ff942bd8c051c1a8a3427380fac65609
Fenil060 <gadhiyafenil090@gmail.com>
2026-03-21 14:15:19 +0530

    add url to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", // your Vite frontend URL
+  origin: "https://book-trade-daoj.vercel.app/", // your Vite frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
